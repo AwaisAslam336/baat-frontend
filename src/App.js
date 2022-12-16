c99d800f3e79524c3880f0e769eee04c6f52938c
Awais Aslam <awaisaslam336@gmail.com>
2022-12-16 23:06:31 +0500

    react-router-dom install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <ChakraProvider>
       <div className="App">hello</div>
-    </ChakraProvider>
   );
 }
 
