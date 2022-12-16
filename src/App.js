2096836e22f3314aa60e63bc11150b61f0dfbe48
Awais Aslam <awaisaslam336@gmail.com>
2022-12-16 23:16:34 +0500

    making seperate components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import { Route } from "react-router-dom";
+import "./App.css";
+import ChatPage from "./Pages/ChatPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-      <div className="App">hello</div>
+    <div className="App">
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
+    </div>
   );
 }
 
