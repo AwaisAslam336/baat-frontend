ef3935fb648aa8dd71ca14b22242edac2c79b9e2
Awais Aslam <awaisaslam336@gmail.com>
2022-12-17 16:43:31 +0500

    HomePage->responsive login/signup layout

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container
+      display={"flex"}
+      maxWidth={["3xl", "4xl", "5xl", "6xl"]}
+      flexDirection={["column", "column", "column", "row"]}
+    >
+      <Text
+        fontSize={["6xl", "7xl", "8xl"]}
+        fontFamily="Work Sans"
+        color={"white"}
+        width="90%"
+        textAlign={"center"}
+        alignSelf={["center", "center", "center", "auto"]}
+      >
+        Baat
+      </Text>
+      <Box
+        bg={"white"}
+        width="90%"
+        h={"min"}
+        p={[3, 4]}
+        borderRadius="lg"
+        borderWidth="1px"
+        alignSelf={["center", "center", "center", "auto"]}
+        m={["30px", "40px", "60px", "70px"]}
+      >
+        <Tabs w="100%" variant="soft-rounded" colorScheme="pink">
+          <TabList mb={"1em"}>
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
