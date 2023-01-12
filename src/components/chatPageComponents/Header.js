127cdcebbcc4ac9bde7f8cd20d1a743a19691bdb
Awais Aslam <awaisaslam336@gmail.com>
2023-01-12 18:54:49 +0500

    ChatPage Header->Search users functionality

--- src/components/chatPageComponents/Header.js
+++ src/components/chatPageComponents/Header.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "@fontsource/rowdies";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, useToast } from "@chakra-ui/react";
 ///////////////////////////////
 import { Search2Icon } from "@chakra-ui/icons";
 //import "@fontsource/rowdies";
 import {
   Drawer,
   DrawerBody,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
@@ -30,19 +29,63 @@ import {
 import { ChatState } from "../../context/ChatProvider";
 import ProfileModal from "../common/ProfileModal";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Loading from "./Loading";
+import UserListItem from "./UserListItem";
 //////////////////////////////////////////////////
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   const { user } = ChatState();
+  const toast = useToast();
   const history = useHistory();
+  const [search, setSearch] = useState();
+  const [searchResult, setSearchResult] = useState();
+  const [loading, setLoading] = useState();
+  //////////////////////
+  async function handleSearch() {
+    if (!search) {
+      toast({
+        title: "Enter search keys to search.",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`api/user?search=${search}`, config);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured",
+        description: "Failed to load search results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  }
+  //////////////////////
   function logoutUser() {
     localStorage.removeItem("userInfo");
     history.push("/");
   }
+  //////////////////////////////////////////////////////
   return (
     <Flex p="0.5rem" bg="white" justifyContent="space-between">
+      {/* Drawer */}
       <>
         <Button ref={btnRef} colorScheme="gray" color="black" onClick={onOpen}>
           <Search2Icon marginRight="0.5rem" />
@@ -57,21 +100,35 @@ const Header = () => {
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>Create your account</DrawerHeader>
+            <DrawerHeader borderBottomWidth={"1px"}>Search Users</DrawerHeader>
 
             <DrawerBody>
-              <Input placeholder="Type here..." />
-            </DrawerBody>
+              <Flex>
+                <Input
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                  placeholder="Search by name or email"
+                />
+                <Button onClick={handleSearch} ml={".5rem"} colorScheme="blue">
+                  Go
+                </Button>
+              </Flex>
 
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="blue">Save</Button>
-            </DrawerFooter>
+              {loading ? (
+                <Loading />
+              ) : (
+                searchResult?.map((searchedUser) => {
+                  return (
+                    <UserListItem key={searchedUser._id} user={searchedUser} />
+                  );
+                })
+              )}
+            </DrawerBody>
           </DrawerContent>
         </Drawer>
       </>
+      {/* Title */}
       <Text
         cursor={"pointer"}
         fontSize="2xl"
@@ -80,6 +137,7 @@ const Header = () => {
       >
         BAAT
       </Text>
+      {/* Right Menu */}
       <div>
         <Menu>
           <MenuButton>
